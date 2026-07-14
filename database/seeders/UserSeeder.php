25c0f7e55b2e058fe54ed12a79785bef5d1efd2a
uhenGit <uhen.dn@gmail.com>
2026-07-14 19:12:56 +0300

    add UserSeeder, create admin and 10 users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Administrator',
+                'password' => Hash::make('password')
+            ]
+        );
+
+        User::factory()->count(10)->create();
+    }
+}
